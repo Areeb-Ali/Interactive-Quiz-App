02c3ba2b1c4f9c05e37e0645caa61af42d1ab356
Areeb-Ali <areebithink@gmail.com>
2024-03-05 17:58:22 +0500

    Complete structure and styling

---
+++ app.js
@@ -0,0 +1,93 @@
+// Adding Questions 
+const questions = [
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+    {
+        question: "First question?",
+        answers: [
+            { text: "option 1", correct: false},
+            { text: "option 2", correct: true}, //True that option which was correct 
+            { text: "option 3", correct: false},
+            { text: "option 4", correct: false},
+        ]
+    },
+]
